9d54be5a7c4b4de6b6702070e0841d24d244932a
Iman Ashoorii <imanashoorii.77@gmail.com>
2022-03-04 20:22:16 +0330

    iniatial commit and first component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ExpenseItem } from "./components/ExpenseItem";
+import { expenses } from "./components/datasource/expenses";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {expenses.map((item) => {
+        return (
+          <ExpenseItem
+            key={item.id}
+            title={item.title}
+            date={item.date}
+            amount={item.amount}
+          />
+        );
+      })}
     </div>
   );
 }
---
+++ src/components/datasource/expenses.js
@@ -0,0 +1,28 @@
+const expenses = [
+  {
+    id: "1",
+    title: "Car Insurance",
+    amount: 5500,
+    date: new Date(),
+  },
+  {
+    id: "2",
+    title: "Buy Ticker",
+    amount: 6500,
+    date: new Date(),
+  },
+  {
+    id: "3",
+    title: "Some foods",
+    amount: 7700,
+    date: new Date(),
+  },
+  {
+    id: "4",
+    title: "Snapp!!",
+    amount: 8000,
+    date: new Date(),
+  },
+];
+
+export { expenses };
